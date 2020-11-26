9b692e0ccb00d07f6d0d1b21d8ce1965f3285047
djbarcelos <djbarcelos@gmail.com>
2020-11-26 10:25:28 -0300

    Google Chrome v-Dev

--- files/script_googleChrome.sh
+++ files/script_googleChrome.sh
@@ -3,10 +3,13 @@
 echo "-------------------------------------------"
 echo "|             Google Chrome               |"
 echo "-------------------------------------------"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-rm -rf ./google-chrome-stable_current_amd64.deb
-clear
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo apt install ./google-chrome-stable_current_amd64.deb
+#rm -rf ./google-chrome-stable_current_amd64.deb
+cd files/Data
+ls
+sudo dpkg -i google-chrome-unstable_current_amd64.deb 
+#clear
 echo "Instalação concluida..."
 read a
 
